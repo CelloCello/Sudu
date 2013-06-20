d23143992aef0c34550d57b7953bcc0959115845
CelloCello <cello1124@gmail.com>
2013-06-20 20:29:26 +0800

    新增article.py來做所有文章發佈、管理相關的功能 新增文章功能試作

--- model/Forms.py
+++ model/Forms.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from flask.ext.wtf import Form, TextField, PasswordField, BooleanField,\
-     SubmitField, required, RadioField, TextAreaField
+     SubmitField, required, RadioField, TextAreaField, FieldList
+
+from flask.ext.wtf import FormField
 
 class LoginForm(Form):
     """
@@ -20,6 +22,20 @@ class RegisterForm(Form):
     repassword = PasswordField(u"確認密碼", validators=[required()])
     submit = SubmitField(u"註冊")
 
+class QuestionForm(Form):
+    """問題表單"""
+    questions = TextAreaField(u"問題", validators=[required()])
+    option_a = TextField(u"選項A", validators=[required()])
+    option_b = TextField(u"選項B", validators=[required()])
+    option_c = TextField(u"選項C", validators=[required()])
+    option_d = TextField(u"選項D", validators=[required()])
+    ans = TextField(u"答案", validators=[required()])
+
+    # def __init__(self, *args, **kwargs):
+    #     kwargs['csrf_enabled'] = False
+    #     super(QuestionForm, self).__init__(*args, **kwargs)
+
+
 class NewArticleForm(Form):
     """
     新增文章用的表單
@@ -27,4 +43,12 @@ class NewArticleForm(Form):
     title = TextField(u"標題", validators=[required()])
     content = TextAreaField(u"內文", validators=[required()])
     authority = RadioField(u"類型", choices=[('value',u'公開'),('value_two',u'私人')])
+    #questions = FieldList(TextAreaField())
+    questions = FieldList(FormField(QuestionForm))
+    # questions = content
+    # for i in xrange(20):
+    #     qu = TextField(u"問題"+str(i), validators=[required()])
+    #     questions.append(content)
+    #     #questions.append_entry()
     submit = SubmitField(u"送出")
+
