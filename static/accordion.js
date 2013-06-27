d215f5ee31765f6dd4c3a225776920c745ae0c9b
CelloCello <cello1124@gmail.com>
2013-06-27 14:22:54 +0800

    將 accordion 展開/收合會造成的問題修正

--- static/accordion.js
+++ static/accordion.js
@@ -2,6 +2,32 @@
 var contentAreas = $('#accordion .ui-accordion-content ').hide();
 var expandLink = $('.accordion-expand-all');
 
+function accordion_refresh(){
+	contentAreas = $('#accordion .ui-accordion-content ').hide();
+	contentAreas.unbind('show');
+	contentAreas.unbind('hide');
+	contentAreas.on({
+	    // whenever we open a panel, check to see if they're all open
+	    // if all open, swap the button to collapser
+	    show: function(){
+	        var isAllOpen = !contentAreas.is(':hidden');   
+	        if(isAllOpen){
+	            expandLink.text('Collapse All')
+	                .data('isAllOpen', true);
+	        }
+	    },
+	    // whenever we close a panel, check to see if they're all open
+	    // if not all open, swap the button to expander
+	    hide: function(){
+	        var isAllOpen = !contentAreas.is(':hidden');
+	        if(!isAllOpen){
+	            expandLink.text('Expand all')
+	            .data('isAllOpen', false);
+	        } 
+	    }
+	});
+}
+
 $(function(){
 	//headers.click(function() {
 	//    var panel = $(this).next();
